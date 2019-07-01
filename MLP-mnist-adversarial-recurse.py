4218c9a3695f2aff19e9c485fa029b81fcaee0a9
Alex Gain <doctorjuice1@gmail.com>
2019-07-01 03:56:32 -0400

    recursion file added

--- MLP-mnist-adversarial-recurse.py
+++ MLP-mnist-adversarial-recurse.py
@@ -485,14 +485,16 @@ for epoch in range(epochs):
         for K in range(args.recurse):
             optimizerDr[K].zero_grad()
             # lossD = ((outputs_advr[K+1] - (outputsr[K] - outputs_advr[K]))**2).mean()
-            lossD = ((outputs_advr[K+1] - (outputsr[K]))**2).mean()
+            lossD = ((outputs_advr[K+1] - outputsr[K])**2).mean()
+            # lossD = ((outputs_advr[K+1] - outputsr[0])**2).mean()
             lossD.backward(retain_graph=True)
-            optimizerDr[K].step()
+            # optimizerDr[K].step()
 
         # for K in range(K1):
-        for K in range(args.recurse):
-            optimizerDr[K].zero_grad()
+        # for K in range(args.recurse):
+            # optimizerDr[K].zero_grad()
             lossD = ((outputsr[K+1] - outputsr[K])**2).mean()
+            # lossD = ((outputsr[K+1] - outputsr[0])**2).mean()
             # lossD = ((outputsr[K+1] - 0)**2).mean()
             lossD.backward(retain_graph=True)
             optimizerDr[K].step()
